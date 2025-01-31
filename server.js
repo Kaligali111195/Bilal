573ff3716a91ce55422f5c87548cf9af626f32d7
Bilal <kratoskingdom7@gmail.com>
2025-02-01 05:11:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 // MongoDB Connection using Mongoose
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    // Removed useUnifiedTopology as it's no longer necessary
 })
     .then(() => {
         console.log("MongoDB Connected");
